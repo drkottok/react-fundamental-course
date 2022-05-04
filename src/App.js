dee3ac181a81cc3cd82494bf2f214d5e2772ef76
Anton Kot <dr.kottok@yandex.ru>
2022-05-04 19:28:08 +0300

    1.0.0 clear app

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import React, {useEffect, useState} from 'react';
-import './styles/App.css';
-import {BrowserRouter} from "react-router-dom";
-import Navbar from "./components/UI/Navbar/Navbar";
-import AppRouter from "./components/AppRouter";
-import {AuthContext} from "./context";
+import React,  from 'react';
+
 
 function App() {
-    const [isAuth, setIsAuth] = useState(false);
-    const [isLoading, setLoading] = useState(true);
-
-    useEffect(() => {
-        if (localStorage.getItem('auth')) {
-            setIsAuth(true)
-        }
-        setLoading(false);
-    }, [])
 
     return (
-        <AuthContext.Provider value={{
-            isAuth,
-            setIsAuth,
-            isLoading
-        }}>
-            <BrowserRouter>
-                <Navbar/>
-                <AppRouter/>
-            </BrowserRouter>
-        </AuthContext.Provider>
-    )
-}
+        <div className="App">
 
+        </div>
+    );
+}
 export default App;
